72c27e556efef73a5b778b824b54b6f827a37614
evo <evo@fearn.li>
2024-06-09 14:15:12 -0400

    Update quartz.layout.ts

--- .quartz/quartz.layout.ts
+++ .quartz/quartz.layout.ts
@@ -30,6 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
 		title: "Navigation", // title of the explorer component
 		folderClickBehavior: "link", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
 		folderDefaultState: "open",
+		useSavedState: false,
 	})),
   ],
   right: [
