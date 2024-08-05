c13a639e696f7a4bc7d66707d4d69556d338646d
evo <evo@fearn.li>
2024-08-05 12:49:02 -0400

    Update quartz.layout.ts

--- .quartz/quartz.layout.ts
+++ .quartz/quartz.layout.ts
@@ -25,17 +25,17 @@ export const defaultContentPageLayout: PageLayout = {
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.Darkmode()
+    Component.Darkmode(),
   ],
   right: [
     Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents())
+    Component.DesktopOnly(Component.TableOfContents()),
   ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta(),],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
@@ -43,6 +43,6 @@ export const defaultListPageLayout: PageLayout = {
     Component.Darkmode(),
   ],
   right: [
-	Component.Graph()
+	Component.Graph(),
   ],
 }
