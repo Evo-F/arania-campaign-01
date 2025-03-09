ff0e08f83a95e35656640bb63ad2e693def93514
Evo Fearnley <evo@fearn.li>
2025-03-09 19:56:06 -0400

    Update quartz.layout.ts

--- .quartz/quartz.layout.ts
+++ .quartz/quartz.layout.ts
@@ -26,14 +26,12 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-	beforeBody: [],
 	left: [],
 	right: [],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-	beforeBody: [],
 	left: [],
 	right: [],
 }
